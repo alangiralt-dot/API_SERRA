14f051b00982d4b7351dbda5d6f0b73f565fc8ee
Alan GIRALT <alangiralt@telefonica.net>
2026-08-20 02:37:17 +0200

    feat: create endpoint to fetch products by category

--- app/Http/Controllers/CatalogueController.php
+++ app/Http/Controllers/CatalogueController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\ChildProduct;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -12,5 +13,26 @@ class CatalogueController extends Controller
     {
         $categories = Category::all();
         return response()->json($categories, 200);
+    }
+    public function showChildProducts(int $id)
+    {
+        $category = Category::findOrFail($id);
+
+        $productsRaw = ChildProduct::with(['fatherProduct', 'availability', 'unit'])
+            ->whereHas('fatherProduct', function ($query) use ($id) {
+                $query->where('category_id', $id);
+            })
+            ->get();
+
+        $productsRaw->makeHidden(['cost_unit_price']);
+        
+        $groupedProducts = $productsRaw->groupBy(function ($product) {
+            return $product->fatherProduct->name;
+        });
+
+        return response()->json([
+            'category' => $category,
+            'products' => $groupedProducts
+        ], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatalogueController;
 
 Route::get('/menu', [CatalogueController::class, 'getMenu']);
+Route::get('/categories/{id}/products', [CatalogueController::class, 'showChildProducts']);
