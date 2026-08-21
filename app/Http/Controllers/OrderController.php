1e031c03ea65727e83100df370ac93d1dc9fe0ab
Alan GIRALT <alangiralt@telefonica.net>
2026-08-21 23:15:40 +0200

    feat: create the endpoint and method that calculates the subtotal of an invoice line based on the price units

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ChildProduct;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+class OrderController extends Controller
+{
+    public function calculateLineSubtotal(Request $request): JsonResponse
+    {
+        $childProductId = $request->input('id');
+        $quantity = $request->input('quantity');
+
+        $product = ChildProduct::with('fatherProduct')->findOrFail($childProductId);
+
+        $subtotal = 0.0;
+        $unitId = $product->unit_id;
+
+        if ($unitId == 1 || $unitId == 4) { 
+            $subtotal = $quantity * $product->current_unit_price;
+        } elseif ($unitId == 2) { 
+            $linearMetres = ($product->length / 1000) * $quantity;
+            $subtotal = $linearMetres * $product->current_unit_price;
+        } elseif ($unitId == 5) { 
+            $squareMetres = (($product->width / 1000) * ($product->length / 1000)) * $quantity;
+            $subtotal = $squareMetres * $product->current_unit_price;
+        } elseif ($unitId == 3) { 
+            $cubicMetres = (($product->width / 1000) * ($product->height / 1000) * ($product->length / 1000)) * $quantity;
+            $subtotal = $cubicMetres * $product->current_unit_price;
+        }
+
+        return response()->json([
+            'subtotal' => round($subtotal, 2)
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CatalogueController;
+use App\Http\Controllers\OrderController;
+
 
 Route::get('/menu', [CatalogueController::class, 'getMenu']);
 Route::get('/categories/{id}/products', [CatalogueController::class, 'showChildProducts']);
+Route::get('/orders/line-subtotal', [OrderController::class, 'calculateLineSubtotal']);
