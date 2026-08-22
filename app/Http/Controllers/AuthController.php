b32395f85875f9430f0ecea4104e8a3b14f9e6cd
Alan GIRALT <alangiralt@telefonica.net>
2026-08-23 01:27:29 +0200

    feat: extract tokens validation logic to LoginRequest form request

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,17 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Http\Requests\LoginRequest;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
-        $credentials = $request->validate([
-            'email'    => ['required', 'email'],
-            'password' => ['required'],
-        ]);
-
+        $credentials = $request->validated();
+        
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
 
